7a563952caa16920c84f698f90f98e05be031588
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-18 22:43:44 -0400

    commit tree c430f92ff808584ad904c0b74ad2984a9b65f04b

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,25 +3,46 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, setItems }) {
+  //set state for the categor and search filters
   const [selectedCategory, setSelectedCategory] = useState("All");
-
+  const [search, setSearched] = useState("");
+  //handlers that will update state for search and category filters
+  function handleSearchChange(event) {
+    setSearched(event.target.value);
+  }
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  //pulls up the object from the ItemForm child adds it to "items" list
+  function onItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
 
-  const itemsToDisplay = items.filter((item) => {
+  //filters through "items" and only returns an item if the category matches whats selected
+  let itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
+  });
+
+  //pulling from the itemsToDisplay list, returns an item the name contains whats typed in the search
+  let filteredItems = itemsToDisplay.filter((item) => {
+    if (search === "") {
+      return true;
+    }
+    return item.name.toLowerCase().includes(search.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+        search={search}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
